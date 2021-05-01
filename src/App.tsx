d2a5022129c7322f537b82c8cf9ef4576905faf1
mecarito <pieritomecar@gmail.com>
2021-05-02 00:01:30 +0300

    added intellisence for tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,20 @@ function App() {
         >
           Learn React
         </a>
+
+        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+          <div className="flex-shrink-0">
+            <img
+              className="h-12 w-12"
+              src="./logo512.png"
+              alt="ChitChat Logo"
+            ></img>
+          </div>
+          <div>
+            <div className="text-xl font-medium text-black">ChitChat</div>
+            <p className="text-gray-500">You have a new message!</p>
+          </div>
+        </div>
       </header>
     </div>
   );
