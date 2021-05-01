f08988230c3840617afc3d6d10c85f84a65ea537
mecarito <pieritomecar@gmail.com>
2021-05-01 19:42:10 +0300

    removed unused css

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p> i love react </p>
+        <p> i love react so much</p>
         <a
           className="App-link"
           href="https://reactjs.org"
