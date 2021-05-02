0c9783ec1530b458bf4ce0f58fd84034ab2ce634
mecarito <pieritomecar@gmail.com>
2021-05-02 21:57:44 +0300

    changed the incoming message

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ export default function App() {
           </div>
           <div>
             <div className="text-xl font-medium text-black">ChitChat</div>
-            <p className="text-gray-500">You have a new message!</p>
+            <p className="text-gray-500">You have an incomming message!</p>
           </div>
         </div>
       </header>
