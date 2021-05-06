036a5cdd2a919ac08d58d5af6870c84a34ad4953
mecarito <pieritomecar@gmail.com>
2021-05-06 14:48:00 +0300

    added some array functions to the list

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
+  const arr = Array(9).fill(8);
+  console.log(arr);
   return (
     <div className="App">
       <header className="App-header">
