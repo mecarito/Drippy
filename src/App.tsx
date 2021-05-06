a7d59b8c266199af942b6c57601b675739e991ac
mecarito <pieritomecar@gmail.com>
2021-05-06 19:04:57 +0300

    removed the logo svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p> i love react so much</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-          <div className="flex-shrink-0">
-            <img
-              className="h-12 w-12"
-              src="./logo512.png"
-              alt="ChitChat Logo"
-            ></img>
-          </div>
-          <div>
-            <div className="text-xl font-medium text-black">ChitChat</div>
-            <p className="text-gray-500">You have an incomming message!</p>
-          </div>
-        </div>
-      </header>
-    </div>
-  );
+export default function App(props: any) {
+  return <h1> Hello {props.name}</h1>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App name="james and sara" />
   </React.StrictMode>,
   document.getElementById("root")
 );
