ca37b00b3712bcaaa5d6caa58aabd8ac8ffa89f6
mecarito <pieritomecar@gmail.com>
2021-05-07 17:05:36 +0300

    fixed issues with state and updated random number

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 import "./App.css";
 
-export default class App extends React.Component<{}, { date: Date }> {
+export default class App extends React.Component<{}, { date: number }> {
   timerID: any;
   constructor(props: any) {
     super(props);
-    this.state = { date: new Date() };
+    this.state = { date: 0 };
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 100);
+    this.timerID = setInterval(() => this.tick(), 1000);
   }
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
-
   tick() {
-    this.setState({ date: new Date() });
+    this.setState({ date: Math.random() });
   }
 
   render() {
     return (
       <div>
         <h1> Today's time</h1>
-        <h1> {this.state.date.toLocaleTimeString()}</h1>
+        <h1> {this.state.date}</h1>
       </div>
     );
   }
