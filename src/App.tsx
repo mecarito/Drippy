fc65f70abcd5bec812568497d65df4420fa790f6
mecarito <pieritomecar@gmail.com>
2021-05-07 23:46:04 +0300

    created a list of strings

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default class App extends React.Component<{}, { counter: number }> {
-  timerID: any;
   constructor(props: any) {
     super(props);
     this.state = { counter: 0 };
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-  tick() {
-    this.setState((state) => ({
-      counter: state.counter + 4,
-    }));
-  }
+  list: any;
+  numbers = [1, 2, 3, 4, 5];
+  listItems = this.numbers.map((number) => (
+    <li key={number.toString()}>{number}</li>
+  ));
 
   render() {
     return (
-      <div>
-        <h1> Today's time</h1>
-        <h1> {this.state.counter}</h1>
-      </div>
+      // <div>
+      <ol>{this.listItems}</ol>
+      // </div>
     );
   }
 }
