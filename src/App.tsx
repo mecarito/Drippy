9199d1aed1ea22e2c20c0c68938b3b68ab485af6
mecarito <pieritomecar@gmail.com>
2021-05-07 16:28:48 +0300

    adding state to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
 import React from "react";
 import "./App.css";
 
-export default function App(props: any) {
-  return <h1> Hello {props.name}</h1>;
+export default class App extends React.Component<any, any> {
+  timerID: any;
+  constructor(props: any) {
+    super(props);
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1> Today's time</h1>
+        <h1> {this.state.date.tolocaleTimeString()}</h1>
+      </div>
+    );
+  }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App name="james and sara" />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
