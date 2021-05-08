0fcefadbc2e538190b623ee48d36bf9d670ee031
mecarito <pieritomecar@gmail.com>
2021-05-08 10:01:14 +0300

    removed the class

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,4 @@
 import React from "react";
 import "./App.css";
 
-export default class App extends React.Component<{}, { counter: number }> {
-  constructor(props: any) {
-    super(props);
-    this.state = { counter: 0 };
-  }
-
-  list: any;
-  numbers = [1, 2, 3, 4, 5];
-  listItems = this.numbers.map((number) => (
-    <li key={number.toString()}>{number}</li>
-  ));
-
-  render() {
-    return (
-      // <div>
-      <ol>{this.listItems}</ol>
-      // </div>
-    );
-  }
-}
+export default class App extends React.Component<{}> {}
