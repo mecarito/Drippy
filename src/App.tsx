b057817a3e55e8f6b5f4233ac7546e882f0022d8
mecarito <pieritomecar@gmail.com>
2021-05-08 10:13:36 +0300

    created form

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,37 @@
 import React from "react";
 import "./App.css";
 
-export default class App extends React.Component<{}> {}
+export default class App extends React.Component<{}, { value: string }> {
+  constructor(props: any) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange() {
+    this.setState({});
+  }
+
+  handleSubmit(e: MouseEvent) {
+    alert(`your name is: ${this.state.value}`);
+    e.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <input
+          type="submit"
+          value="submit"
+          onSubmit={(e) => this.handleSubmit}
+        />
+      </div>
+    );
+  }
+}
