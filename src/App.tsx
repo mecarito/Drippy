561c4d6da39023a33003acc2ce21d22c6b1a9017
mecarito <pieritomecar@gmail.com>
2021-05-15 15:37:35 +0300

    added tailwind component

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,66 @@
-import React, { Suspense } from "react";
+import React from "react";
 import "./App.css";
 
-// interface Form {
-//   value: string;
-//   age: number;
+// export default className App extends React.Component{
+
 // }
-
-export default class App extends React.Component<
-  {},
-  { name: string; age: number }
-> {
-  constructor(props: any) {
-    super(props);
-    this.state = { name: "", age: 0 };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event: any) {
-    if (event.target.type === "text") {
-      this.setState({ name: event.target.value });
-    } else {
-      this.setState({ age: event.target.value });
-    }
-
-    console.log(this.state.name, this.state.age);
-  }
-
-  handleSubmit(event: any) {
-    event.preventDefault();
-    alert(`your name is: ${this.state.name}, age is ${this.state.age}`);
-    const student = {
-      name: "peter",
-      age: 16,
-    };
-    console.log(student["name"]);
-  }
-  render() {
-    return (
-      // <form onSubmit={this.handleSubmit}>
-      //   <label htmlFor="name">
-      //     Name:
-      //     <input
-      //       type="text"
-      //       value={this.state.name}
-      //       onChange={this.handleChange}
-      //     />
-      //   </label>
-      //   <br />
-      //   <label htmlFor="age">
-      //     Age:
-      //     <input
-      //       type="number"
-      //       onChange={this.handleChange}
-      //       // value={this.state.age}
-      //     />
-      //   </label>
-      //   <br />
-      //   <input type="submit" value="submit" />
-      // </form>
-      <Sidebar>
-        <h1> i love react</h1>
-        <h2> the web is great</h2>
-      </Sidebar>
-    );
-  }
-}
-function Sidebar(props: any) {
-  return (
-    <div>
-      <p> This is a sidebar</p>
-      {props.children}
+export default function App(){
+  return <div className="flex">
+  <div className="flex-none w-48 relative">
+    <img src="/classNameic-utility-jacket.jpg" alt="" className="absolute inset-0 w-full h-full object-cover" />
+  </div>
+  <form className="flex-auto p-6">
+    <div className="flex flex-wrap">
+      <h1 className="flex-auto text-xl font-semibold">
+        classNameic Utility Jacket
+      </h1>
+      <div className="text-xl font-semibold text-gray-500">
+        $110.00
+      </div>
+      <div className="w-full flex-none text-sm font-medium text-gray-500 mt-2">
+        In stock
+      </div>
     </div>
-  );
+    <div className="flex items-baseline mt-4 mb-6">
+      <div className="space-x-2 flex">
+        <label>
+          <input className="w-9 h-9 flex items-center justify-center bg-gray-100 rounded-lg" name="size" type="radio" value="xs" checked>
+          XS
+        </label>
+        <label>
+          <input className="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="s">
+          S
+        </label>
+        <label>
+          <input className="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="m">
+          M
+        </label>
+        <label>
+          <input className="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="l">
+          L
+        </label>
+        <label>
+          <input className="w-9 h-9 flex items-center justify-center" name="size" type="radio" value="xl">
+          XL
+        </label>
+      </div>
+      <div className="ml-auto text-sm text-gray-500 underline">Size Guide</div>
+    </div>
+    <div className="flex space-x-3 mb-4 text-sm font-medium">
+      <div className="flex-auto flex space-x-3">
+        <button className="w-1/2 flex items-center justify-center rounded-md bg-black text-white" type="submit">Buy now</button>
+        <button className="w-1/2 flex items-center justify-center rounded-md border border-gray-300" type="button">Add to bag</button>
+      </div>
+      <button className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-gray-400 border border-gray-300" type="button" aria-label="like">
+        <svg width="20" height="20" fill="currentColor">
+          <path fill-rule="evenodd" clip-rule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" />
+        </svg>
+      </button>
+    </div>
+    <p className="text-sm text-gray-500">
+      Free shipping on all continental US orders.
+    </p>
+  </form>
+</div>
+
 }
