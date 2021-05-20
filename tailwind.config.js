05ba65a3cf8159f71500116c32ba4a8e917d232f
mecarito <pieritomecar@gmail.com>
2021-05-20 07:54:02 +0300

    added just in time compilation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 module.exports = {
+    mode: "jit",
     purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
     },
-    //   variants: {
-    //     extend: {},
-    //   },
-    // variants: {
-    //     extend: {
-    //         padding: ["hover"],
-    //     },
-    // },
+    variants: {
+        extend: {},
+    },
     plugins: [require("@tailwindcss/forms")],
 };
