862bba371ef5a53cb5c760de5cb1d299bc7396e2
mecarito <pieritomecar@gmail.com>
2021-05-21 14:45:05 +0300

    removed unused form elements

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default class App extends React.Component<{}, {}> {
-  inputref: React.RefObject<unknown>;
+  inputref: React.RefObject<HTMLInputElement>;
 
   constructor(props: string) {
     super(props);
     this.inputref = React.createRef();
+    this.onsave = this.onsave.bind(this);
+  }
+
+  onsave() {
+    console.log(this.inputref.current?.value);
   }
 
   render() {
@@ -25,7 +30,7 @@ export default class App extends React.Component<{}, {}> {
               </div>
             </div>
             <div className="mt-5 md:mt-0 md:col-span-2">
-              <form action="#" method="POST">
+              <form>
                 <div className="shadow overflow-hidden sm:rounded-md">
                   <div className="px-4 py-5 bg-white sm:p-6">
                     <div className="grid grid-cols-6 gap-6">
@@ -40,6 +45,7 @@ export default class App extends React.Component<{}, {}> {
                           type="text"
                           defaultValue="peter mecar"
                           name="first_name"
+                          ref={this.inputref}
                           id="first_name"
                           autoComplete="given-name"
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
@@ -61,127 +67,21 @@ export default class App extends React.Component<{}, {}> {
                           className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         />
                       </div>
-
-                      <div className="col-span-6 sm:col-span-4">
-                        <label
-                          htmlFor="email_address"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Email
-                        </label>
-                        <input
-                          type="text"
-                          name="email_address"
-                          id="email_address"
-                          autoComplete="email"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-3">
-                        <label
-                          htmlFor="country"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Country / Region
-                        </label>
-                        <select
-                          id="country"
-                          name="country"
-                          autoComplete="country"
-                          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                        >
-                          <option>United States</option>
-                          <option>Canada</option>
-                          <option>Mexico</option>
-                        </select>
-                      </div>
-
-                      <div className="col-span-6">
-                        <label
-                          htmlFor="street_address"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          Street address
-                        </label>
-                        <input
-                          type="text"
-                          name="street_address"
-                          id="street_address"
-                          autoComplete="street-address"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-6 lg:col-span-2">
-                        <label
-                          htmlFor="city"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          City
-                        </label>
-                        <input
-                          type="text"
-                          name="city"
-                          id="city"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                        <label
-                          htmlFor="state"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          State / Province
-                        </label>
-                        <input
-                          type="text"
-                          name="state"
-                          id="state"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
-
-                      <div className="col-span-6 sm:col-span-3 lg:col-span-2">
-                        <label
-                          htmlFor="postal_code"
-                          className="block text-sm font-medium text-gray-700"
-                        >
-                          ZIP / Postal
-                        </label>
-                        <input
-                          type="text"
-                          name="postal_code"
-                          id="postal_code"
-                          autoComplete="postal-code"
-                          className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                        />
-                      </div>
                     </div>
                   </div>
                   <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                     <button
                       type="submit"
+                      onClick={this.onsave}
                       className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                       Save
                     </button>
                   </div>
                 </div>
-                <input
-                  type="checkbox"
-                  className="appearance-none checked:bg-blue-600 checked:border-transparent ..."
-                />
               </form>
             </div>
           </div>
-        </div>
-        <div className="grid grid-row-2">
-          <div className="col-end-2">1</div>
-          <div>2</div>
-          <div>3</div>
-          <div className="col-end-2">4</div>
         </div>
       </>
     );
