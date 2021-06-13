96bc19d064ebaa02ee63ef60ada5ee4dbc37f4f9
mecarito <pieritomecar@gmail.com>
2021-06-14 00:49:05 +0300

    added support for picking devices

--- src/App.tsx
+++ src/App.tsx
@@ -16,19 +16,26 @@ export default function App() {
   }
 
   async function test() {
-    try {
-      const device = await navigator.bluetooth.requestDevice({
-        filters: [
-          {
-            namePrefix: "test",
-          },
-        ],
-        optionalServices: ["test"],
+    let options = {
+      filters: [
+        { services: ["heart_rate"] },
+        { services: [0x1802, 0x1803] },
+        { services: ["c48e6067-5295-48d3-8d5c-0395f61792b1"] },
+        { name: "ExampleName" },
+        { namePrefix: "Prefix" },
+      ],
+      optionalServices: ["battery_service"],
+    };
+
+    navigator.bluetooth
+      .requestDevice(options)
+      .then(function (device) {
+        console.log("Name: " + device.name);
+        // Do something with the device.
+      })
+      .catch(function (error) {
+        console.log("Something went wrong. " + error);
       });
-      console.log(device);
-    } catch (error) {
-      console.error(error);
-    }
   }
 
   return (
