c77dacaef897088b0c9f2491a4ce61fac759dbd0
mecarito <pieritomecar@gmail.com>
2021-06-14 01:15:26 +0300

    scan all devices

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,9 @@ import "./App.css";
 export default function App() {
   function connection() {
     navigator.bluetooth
-      .requestDevice()
-      .then((device) => {
-        /* … */
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  async function test() {
-    navigator.bluetooth
       .requestDevice({
-        filters: [
-          {
-            name: "E6",
-          },
-        ],
-        // optionalServices: ["battery_service"], // Required to access service later.
+        acceptAllDevices: true,
+        optionalServices: ["battery_service"], // Required to access service later.
       })
       .then((device) => {
         /* … */
@@ -40,15 +25,9 @@ export default function App() {
       </h1>
       <button
         className="py-4 px-16 text-white bg-indigo-700 w-15 text-lg rounded-lg"
-        onClick={test}
-      >
-        My device
-      </button>
-      <button
-        className="py-4 px-16 text-white bg-indigo-700 w-15 text-lg rounded-lg"
         onClick={connection}
       >
-        All
+        All devices
       </button>
     </div>
   );
