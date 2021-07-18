3260c7fda7e122d0a19984aa22508987e827855c
mecarito <pieritomecar@gmail.com>
2021-07-18 07:01:05 +0300

    removed bluetooth connections code

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
-/// <reference types="web-bluetooth" />
-
 import React, { useEffect } from "react";
 import "./App.css";
 
 export default function App() {
   useEffect(() => {
-    connection();
     return () => {
       // cleanup
     };
   }, []);
-
-  function connection() {
-    navigator.bluetooth
-      .requestDevice({
-        acceptAllDevices: true,
-        // optionalServices: ["battery_service"], // Required to access service later.
-      })
-      .then((device) => {
-        alert(device);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
-
-  return (
-    <div className="flex flex-col gap-8 items-center">
-      <h1 className="text-indigo-700 text-3xl font-semibold">
-        Bluetooth connection
-      </h1>
-      <button
-        className="py-4 px-16 text-white bg-indigo-700 w-15 text-lg rounded-lg"
-        onClick={connection}
-      >
-        All devices
-      </button>
-    </div>
-  );
 }
