d116a79492cd29b7bc65ad7f86eb58ddd7ac4f75
mecarito <pieritomecar@gmail.com>
2021-07-18 08:02:25 +0300

    created a redux store, and dispatched some actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./counterSlice";
 
 export default function App() {
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="flex flex-col gap-8 items-center">
       <h1 className="text-indigo-700 text-3xl font-semibold">
         redux shopping cart
       </h1>
+
+      <div>
+        <div className="flex flex-col gap-y-6">
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
