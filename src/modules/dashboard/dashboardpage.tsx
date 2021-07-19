0ee188a7d2a7cf8385d7fa09969d9257cb94492e
mecarito <pieritomecar@gmail.com>
2021-07-19 20:47:47 +0300

    added dashboard ui

--- src/modules/dashboard/dashboardpage.tsx
+++ src/modules/dashboard/dashboardpage.tsx
@@ -20,6 +20,21 @@ export default function Dashboardpage() {
           <h1 className="text-2xl font-medium  text-black">Today</h1>
           <p className="text-sm text-gray-500">{Todaydate}</p>
         </div>
+        <div>
+          {tasks.map((task) => {
+            return (
+              <div
+                key={task}
+                className="flex flex-row space-x-4 content-start mb-2"
+              >
+                <div>
+                  <div className="w-4 h-4 rounded-full ring-1 ring-gray-500"></div>
+                </div>
+                <p className="overflow-y-auto">{task}</p>
+              </div>
+            );
+          })}
+        </div>
         <button
           className="flex flex-row space-x-2 focus:outline-none
         cursor-pointer "
@@ -28,15 +43,6 @@ export default function Dashboardpage() {
           <span className="material-icons text-primary">add</span>
           <p className="text-gray-500 hover:text-primary">Add task</p>
         </button>
-        <div>
-          {tasks.map((task) => {
-            return (
-              <div key={task}>
-                <h1>{task}</h1>
-              </div>
-            );
-          })}
-        </div>
       </div>
     </div>
   );
